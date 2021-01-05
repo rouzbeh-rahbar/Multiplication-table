1f8e013d0021df223582eae719dfdb5769260762
rouzbeh-rahbar <rouzbeh.rahbar4@gmail.com>
2021-01-05 13:05:22 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n=10 , m=10 , x=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=10; j++){
+                x=i*j;
+                System.out.print(" " + x);
+
+            }
+            System.out.println(" ");
+        }
+
+
+
+    }
+}
